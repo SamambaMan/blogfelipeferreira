636435210f231a4eb103b2083b61afa937a9f3b2
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-10-03 17:03:17 -0300

    Adicao do suporte a Boto S3 para media files no heroku

--- blogfelipeferreira/settings.py
+++ blogfelipeferreira/settings.py
@@ -160,3 +160,10 @@ if "AMBIENTE" in os.environ and os.environ["AMBIENTE"] == "heroku":
     }
 
     ALLOWED_HOSTS = ['*']
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = '%s/media/' % AWS_S3_CUSTOM_DOMAIN
