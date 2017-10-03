a7087c14a48053f9b9cc76f2d14b75469521dc43
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-10-03 17:41:47 -0300

    adicao de um media storage para o path de midia ser completo

--- blogfelipeferreira/settings.py
+++ blogfelipeferreira/settings.py
@@ -161,7 +161,7 @@ if "AMBIENTE" in os.environ and os.environ["AMBIENTE"] == "heroku":
 
     ALLOWED_HOSTS = ['*']
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'blogfelipeferreira.blog.storages.mediastorage.MediaStorage'
     AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
     AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
